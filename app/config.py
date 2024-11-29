403ba7722c2a54712b2aa70b50530dd1823cc873
kali <kali@kali>
2024-11-29 17:58:50 +0300

    change db location

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ class Config:
     SECRET_KEY = 'b3baa1cb519a5651c472d1afa1b3f4e04f1adf6909dae88a4cd39adc0ddd9732'
     # SQLALCHEMY_DATABASE_URI = 'mysql:///std_2414_exam.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://user:55555@localhost:3306/std_2414_exam'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://user:55555@db:3306/std_2414_exam'
     UPLOAD_FOLDER = "./static/covers"
 
 
